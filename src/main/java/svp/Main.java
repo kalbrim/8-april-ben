db66ee8bbcc133d0a182e08cb0b121b51926b9cc
Brendan Comiskey <comiskeybrendan@gmail.com>
2023-04-08 11:16:58 -0400

    polite start

---
+++ src/main/java/svp/Main.java
@@ -0,0 +1,22 @@
+package svp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("koepka again");
+    }
+
+//    public int[] runningSum(int[] nums) {
+//
+//        // arr to return
+//        int [] result = new int[nums.length];
+//        // nothjing doing with first number
+//        // this means loop will starta at 2nd postion aka 1
+//        result[0] = nums[0];
+//        for(int i = 1; i < nums.length; i++){
+//            // take the previous and add current
+//            result[i] = result[i-1] + nums[i];
+//
+//        }
+//        return result;
+//    }
+}
